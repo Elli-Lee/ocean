eb8478c027b7af23b31eb2e0d0ba28b8f218592c
이유진 <elli.stdev@gmail.com>
2025-01-01 17:22:51 +0900

    feat: Edit 페이지 UI, 기능 구현

--- one-bite-react/emotion-diary/src/components/Editor.jsx
+++ one-bite-react/emotion-diary/src/components/Editor.jsx
@@ -1,7 +1,7 @@
 import Button from "./Button";
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 const emotionList = [
   {
@@ -42,7 +42,7 @@ const getStringedDate = (targetDate) => {
   return `${year}-${month}-${date}`;
 };
 
-const Editor = ({ onSubmit }) => {
+const Editor = ({ initData, onSubmit }) => {
   const nav = useNavigate();
   const [input, setInput] = useState({
     createdDate: new Date(),
@@ -50,6 +50,14 @@ const Editor = ({ onSubmit }) => {
     content: "",
   });
 
+  useEffect(() => {
+    if (initData) {
+      setInput({
+        ...initData,
+        createdDate: new Date(Number(initData.createdDate)),
+      });
+    }
+  }, [initData]);
   const onChangeInput = (e) => {
     let name = e.target.name;
     let value = e.target.value;

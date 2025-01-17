358022653e20b18e6403bec805c09a37db97e4f8
이유진 <elli.stdev@gmail.com>
2025-01-17 20:14:28 +0900

    feat: component 나누기

--- front-end/react-complete/learn-react/src/App.jsx
+++ front-end/react-complete/learn-react/src/App.jsx
@@ -1,48 +1,12 @@
 import "./App.css";
+import CourseListCard from "./components/course/CourseListCard";
 
 function App() {
   return (
     <>
-      <div className="card">
-        <div className="card__header">강의 목록</div>
-        <div className="card__body">
-          <div className="courses">
-            <article className="course">
-              <img className="course__img" src="./img/htmlcss.png" alt="" />
-              <div className="course__body">
-                <div className="course__title">
-                  입문자를 위한, HTML&CSS 웹 개발 입문
-                </div>
-                <div className="course__description">
-                  웹 개발에 필요한 기본 지식을 배웁니다.{" "}
-                </div>
-              </div>
-            </article>
-            <article className="course">
-              <img className="course__img" src="./img/htmlcss.png" alt="" />
-              <div className="course__body">
-                <div className="course__title">
-                  입문자를 위한, HTML&CSS 웹 개발 입문
-                </div>
-                <div className="course__description">
-                  웹 개발에 필요한 기본 지식을 배웁니다.{" "}
-                </div>
-              </div>
-            </article>
-            <article className="course">
-              <img className="course__img" src="./img/htmlcss.png" alt="" />
-              <div className="course__body">
-                <div className="course__title">
-                  입문자를 위한, HTML&CSS 웹 개발 입문
-                </div>
-                <div className="course__description">
-                  웹 개발에 필요한 기본 지식을 배웁니다.{" "}
-                </div>
-              </div>
-            </article>
-          </div>
-        </div>
-      </div>
+      <main>
+        <CourseListCard />
+      </main>
     </>
   );
 }

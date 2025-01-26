587d20b217a3ce25735c5862a5d5a44e636222c5
이유진 <elli.stdev@gmail.com>
2025-01-26 18:35:58 +0900

    feat: react state example

--- front-end/react/react-complete/learn-react/src/components/Counter.jsx
+++ front-end/react/react-complete/learn-react/src/components/Counter.jsx
@@ -1,5 +1,15 @@
-const Counter = () => {
-  return <button> counter </button>;
+import { useState } from "react";
+
+const Counter = ({ onTotal }) => {
+  const [counter, setCounter] = useState(0);
+  const handleCounter = () => {
+    setCounter(counter + 1);
+    if (onTotal) {
+      onTotal();
+    }
+  };
+
+  return <button onClick={handleCounter}> counter: {counter} </button>;
 };
 
 export default Counter;
--- front-end/react/react-complete/learn-react/src/components/Main.jsx
+++ front-end/react/react-complete/learn-react/src/components/Main.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import Counter from "./Counter";
 
 const Main = () => {
+  const [total, setTotal] = useState(0);
+  const [flag, setFlag] = useState(false);
+
+  const handleTotal = () => {
+    setTotal(total + 1);
+  };
+
   return (
     <main>
+      <h2>Total: {total}</h2>
+      <h2>Flag: {flag.toString()}</h2>
+      <button onClick={() => setFlag(!flag)}> toggle flag </button>
+      <br />
+      <Counter onTotal={handleTotal} />
+      <hr />
+      <Counter onTotal={handleTotal} />
+      <hr />
       <Counter />
     </main>
   );
